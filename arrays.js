5b95e53e7890d7a92175fcc75af3ac73a73c4365
conztantin <conztantinsixten@gmail.com>
2025-02-03 09:03:52 +0100

    opgave 1 + 2

--- arrays.js
+++ arrays.js
@@ -4,6 +4,14 @@ document.addEventListener("DOMContentLoaded", function() {
     let fruitsArray = ["Banana", "Orange", "Pineapple", "Mango", "Plum", "Apricot"]
     let numbersArray = [12, 4, 45, 17, 2, 26, 7, 55, 48, 32, 21]
 
-    // --- Løs opgaverne herunder --- 
+    // --- Løs opgaverne herunder ---
+
+    //opgave 1
+let filteredNumbers = numbersArray.filter(num => num > 20);
+console.log(filteredNumbers); 
+
+//opgave 2
+catsArray.push("Sniffles");
+console.log(catsArray)
 
 }) // ends DOMContentLoaded
